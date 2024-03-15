313c08a2305c568eae051216077b09e0149de850
zebubull <ldkourtev@gmail.com>
2024-03-14 23:43:57 -0400

    refactor: remove v1 code
    Removed leftover code from app v1 and reset to prepare for v2.
    
    BREAKING CHANGE: Removed all app functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
-import 'package:scouting_app/new_team_page.dart';
-import 'package:scouting_app/share_page.dart';
-import 'package:scouting_app/team_page.dart';
-import 'package:scouting_app/team_select_page.dart';
-import 'package:scouting_app/teams_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-
-import 'big_card.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,118 +16,13 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) {
-          final model = TeamsModel();
-          model.load();
-          return model;
-        }),
-      ],
-      child: MaterialApp(
-        title: 'Gamer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
-        ),
-        home: HomePage(),
+    return MaterialApp(
+      title: '8873 Scouting',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
       ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var selectedIndex = 0;
-  var selectedTeam = 0;
-  
-  @override
-  Widget build(BuildContext context) {
-    Widget page = switch (selectedIndex) {
-      0 => MainPage(),
-      1 => TeamSelectPage(
-        onTeamSelect: (id) => setState(() {
-          selectedTeam = id;
-          selectedIndex = 4;
-      })),
-      2 => NewTeamPage(
-        onTeamCreated: (id) => setState(() {
-          selectedTeam = id;
-          selectedIndex = 4;
-      })),
-      3 => SharePage(),
-      4 => TeamPage(id: selectedTeam),
-      _ => throw UnimplementedError('Page with index $selectedIndex not found!'),
-    };
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: _makeNavRail(constraints),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-
-  Consumer<TeamsModel> _makeNavRail(BoxConstraints constraints) {
-    return Consumer<TeamsModel>(
-      builder: (context, teams, _) => NavigationRail(
-        extended: constraints.maxWidth >= 700,
-        destinations: [
-          NavigationRailDestination(icon: Icon(Icons.home), label: Text('Home')),
-          NavigationRailDestination(icon: Icon(Icons.menu), label: Text('Teams')),
-          NavigationRailDestination(icon: Icon(Icons.add), label: Text('New Team')),
-          NavigationRailDestination(icon: Icon(Icons.share), label: Text('Share')),
-          NavigationRailDestination(
-            icon: Icon(teams.saved ? Icons.check_circle_outline : Icons.save_outlined),
-            label: Text(teams.saved ? 'Changes Saved' : 'Unsaved Changes'),
-          ),
-          NavigationRailDestination(icon: Icon(Icons.archive), label: Text('Export Data')),
-        ],
-        selectedIndex: selectedIndex == 4 ? 1 : selectedIndex,
-        onDestinationSelected: (dest) {
-          if (dest == 4) {
-            Provider.of<TeamsModel>(context, listen: false).save(context);
-          } else if (dest == 5) {
-            Provider.of<TeamsModel>(context, listen: false).export(context);
-          } else {
-            setState(() => selectedIndex = dest);
-          }
-        },
-      ),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(text: "Scouting App"),
-          SizedBox(height: 10),
-          Image(image: AssetImage('assets/biden.jpg')),
-          SizedBox(height: 10),
-          const Text("Sponsored by Sleepy Joe Brandon"),
-        ]
-      )
+      home: Scaffold(),
     );
   }
 }
