8b89a86c994ec865646d639a596e4909fdfb3851
zebubull <ldkourtev@gmail.com>
2024-03-15 01:24:15 -0400

    feat(scouter): Add navigation bar
    Added a simple navigation bar the the scouter home page.

--- lib/views/scouter/scouter.dart
+++ lib/views/scouter/scouter.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
-class ScouterView extends StatelessWidget {
+class ScouterView extends StatefulWidget {
+  @override
+  State<ScouterView> createState() => _ScouterViewState();
+}
+
+class _ScouterViewState extends State<ScouterView> {
+  int currentPageIndex = 0;
+
+  final List<String> pageNames = <String>['Scout', 'Connect'];
+  int get logoutIndex => pageNames.length;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Scouter')),
+      appBar: AppBar(
+        title: Text(pageNames[currentPageIndex]),
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: makeNavigationBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: const Text('Scouter'),
       ),
     );
+  }
+
+  Widget makeNavigationBar() {
+    return NavigationBar(
+      onDestinationSelected: updateSelectedPage,
+      selectedIndex: currentPageIndex,
+      destinations: <Widget>[
+        NavigationDestination(
+          selectedIcon: const Icon(Icons.edit),
+          icon: const Icon(Icons.edit_outlined),
+          label: 'Scout',
+        ),
+        NavigationDestination(
+          selectedIcon: const Icon(Icons.cast_connected_rounded),
+          icon: const Icon(Icons.cast_connected_outlined),
+          label: 'Connect',
+        ),
+        NavigationDestination(
+          icon: const Icon(Icons.logout_rounded),
+          label: 'Logout',
+        ),
+      ],
+    );
+  }
+
+  void updateSelectedPage(int index) {
+    if (index == logoutIndex) {
+      Navigator.pop(context);
+      return;
+    }
+
+    setState(() => currentPageIndex = index);
   }
 }
